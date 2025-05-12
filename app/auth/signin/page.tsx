03713d7f48f7012da4728b1e2879b61ead28989a
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-12 16:03:12 +0200

    feat(signin/page.tsx): replace inline form elements with SignInForm component to improve code organization and reusability

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import PhotoBy from "@/app/components/auth/common/photoBy/photoBy";
 import Link from "next/link";
-import Input from "@/app/components/auth/common/input/input";
-import Button from "@/app/components/auth/common/button/button";
+import SignInForm from "@/app/components/auth/signInForm/signInForm";
 export default function SignIn() {
   return (
     <div className="flex w-full h-screen">
@@ -28,38 +27,21 @@ export default function SignIn() {
             <p className="text-gray-500 mb-4">
               Welcome back! Let&apos;s continue your journey
             </p>
-            <form className="flex flex-col gap-4 w-full">
-              {/* Email */}
-              <Input
-                label="Email"
-                type="email"
-                name="email"
-                placeholder="Email"
-              />
-              {/* Password */}
-              <Input
-                label="Password"
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
-              <Button>Sign in</Button>
-
-              <div className="text-gray-500 text-sm flex justify-between">
-                <div>
-                  Are you new here?{" "}
-                  <Link
-                    href="/auth/signup"
-                    className="font-bold text-black underline"
-                  >
-                    Sign up
-                  </Link>
-                </div>
-                <div className="flex text-sm text-black font-semibold underline">
-                  <Link href="/auth/forgot-password">Forgot password?</Link>
-                </div>
+            <SignInForm />
+            <div className="text-gray-500 text-sm flex justify-between">
+              <div>
+                Are you new here?{" "}
+                <Link
+                  href="/auth/signup"
+                  className="font-bold text-black underline"
+                >
+                  Sign up
+                </Link>
               </div>
-            </form>
+              <div className="flex text-sm text-black font-semibold underline">
+                <Link href="/auth/forgot-password">Forgot password?</Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
