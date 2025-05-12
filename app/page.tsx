fbfd1383f72a54825604d40813156da82cdaa47a
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-12 10:33:08 +0200

    feat(layout.tsx): wrap children in Providers component to manage context and state style(page.tsx): add max-width and flex properties to main for better layout and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="max-w-screen-2xl mx-auto flex flex-col gap-4">
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl font-bold">Hello World</h1>
         <Link href="/status">Status</Link>
