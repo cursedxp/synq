827e3e18d5a0f8b9f035b2b24922271db9ab7840
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-12 16:02:51 +0200

    feat(signin.schema.ts): add signInSchema for validating user sign-in input to ensure data integrity and improve error handling

---
+++ app/schemas/signin/signin.schema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const signInSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+  password: z.string().min(8, "Invalid credentials"),
+});
