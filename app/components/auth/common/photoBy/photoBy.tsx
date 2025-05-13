d55bd32489aeaefe3705a5548877e17b850c5de9
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-13 11:33:01 +0200

    feat(photoBy.tsx): implement toggle functionality for displaying photographer info to enhance user interaction fix(photoBy.tsx): add event listener to close info when clicking outside the component for better usability

--- app/components/auth/common/photoBy/photoBy.tsx
+++ app/components/auth/common/photoBy/photoBy.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { FiMapPin, FiAperture } from "react-icons/fi";
 import Link from "next/link";
 import React from "react";
+import { useState, useRef, useEffect } from "react";
 
 interface PhotoByProps {
   location: string;
@@ -13,25 +15,57 @@ export default function PhotoBy({
   photographer,
   photoUrl,
 }: PhotoByProps) {
+  const [showContent, setShowContent] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    setShowContent(!showContent);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        contentRef.current &&
+        !contentRef.current.contains(event.target as Node)
+      ) {
+        setShowContent(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   return (
-    <div className="absolute bottom-4 right-4 py-2 px-2 rounded-full shadow-lg bg-zinc-900 max-w-md group cursor-pointer">
-      <FiAperture
-        aria-hidden="true"
-        className="text-white group-hover:hidden"
-      />
-      <div className="justify-center text-sm items-center gap-2 mx-2 text-white hidden group-hover:flex group-hover:transition-all group-hover:duration-300">
-        <FiMapPin aria-hidden="true" />
-        <div>{location}</div>
-        <FiAperture aria-hidden="true" />
-        <Link
-          className="font-semibold underline"
-          href={photoUrl}
-          target="_blank"
-          aria-label={`Photo by ${photographer}`}
+    <div
+      className="absolute bottom-4 right-4 py-2 px-2 rounded-full shadow-lg bg-zinc-900 max-w-md cursor-pointer group"
+      onClick={handleClick}
+    >
+      {!showContent ? (
+        <FiAperture
+          aria-hidden="true"
+          className="text-white group-hover:rotate-360 transition-all duration-400"
+        />
+      ) : (
+        <div
+          className={`flex justify-center text-sm items-center gap-2 mx-2 text-white `}
+          ref={contentRef}
         >
-          {photographer}
-        </Link>
-      </div>
+          <FiMapPin aria-hidden="true" />
+          <div>{location}</div>
+          <FiAperture aria-hidden="true" />
+          <Link
+            className="font-semibold underline"
+            href={photoUrl}
+            target="_blank"
+            aria-label={`Photo by ${photographer}`}
+          >
+            {photographer}
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
