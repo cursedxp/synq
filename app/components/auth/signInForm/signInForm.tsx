2c487bf2e4c0db7fcfaeec56c1d4eddbfd157fcd
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-13 16:32:10 +0200

    feat(signInForm): enhance sign-in form with authentication hooks and navigation links
    - Integrate useAuth hook for handling sign-in logic and loading state.
    - Add navigation links for sign-up and password recovery to improve user experience.
    - Display error messages to inform users of sign-in issues.

--- app/components/auth/signInForm/signInForm.tsx
+++ app/components/auth/signInForm/signInForm.tsx
@@ -4,13 +4,16 @@ import Input from "../common/input/input";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signInSchema } from "@/app/schemas/signin/signin.schema";
-
+import { useAuth } from "@/app/hooks/useAuth";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 interface SignInFromData {
   email: string;
   password: string;
 }
 
 export default function SignInForm() {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -19,32 +22,49 @@ export default function SignInForm() {
     resolver: zodResolver(signInSchema),
   });
 
+  const { signIn, error, loading } = useAuth();
+
   const onSubmit = (data: SignInFromData) => {
-    console.log(data);
+    signIn(data);
+    router.push("/");
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col gap-4 w-full"
-    >
-      {/* Email */}
-      <Input
-        label="Email"
-        type="email"
-        placeholder="Email"
-        error={errors.email?.message}
-        {...register("email")}
-      />
-      {/* Password */}
-      <Input
-        label="Password"
-        type="password"
-        placeholder="Password"
-        error={errors.password?.message}
-        {...register("password")}
-      />
-      <Button label="Login" />
-    </form>
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 w-full"
+      >
+        {/* Email */}
+        <Input
+          label="Email"
+          type="email"
+          placeholder="Email"
+          error={errors.email?.message}
+          {...register("email")}
+        />
+        {/* Password */}
+        <Input
+          label="Password"
+          type="password"
+          placeholder="Password"
+          error={errors.password?.message}
+          {...register("password")}
+        />
+        <Button label="Login" loading={loading} />
+      </form>
+      <div className="text-gray-500 text-sm flex justify-between">
+        <div>
+          Are you new here?{" "}
+          <Link href="/auth/signup" className="font-bold text-black underline">
+            Sign up
+          </Link>
+        </div>
+        <div className="flex text-sm text-black font-semibold underline">
+          <Link href="/auth/forgot-password">Forgot password?</Link>
+        </div>
+      </div>
+      {error && <p className="text-red-500">{error}</p>}
+    </>
   );
 }
