f008dda4a88b8aad7ef9592def23174881eca8ec
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-13 11:01:20 +0200

    feat(button): refactor Button component to accept label prop and spread button attributes for better flexibility and usability

--- app/components/auth/common/button/button.tsx
+++ app/components/auth/common/button/button.tsx
@@ -1,10 +1,16 @@
-export default function Button({ children }: { children: React.ReactNode }) {
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+}
+
+export default function Button({ label, ...props }: ButtonProps) {
   return (
     <button
-      type="submit"
       className="bg-black text-white p-2 rounded-md mt-2 cursor-pointer hover:scale-[101%] hover:shadow-xl transition-all duration-300 flex justify-center items-center"
+      {...props}
     >
-      {children}
+      {label}
     </button>
   );
 }
