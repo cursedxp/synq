1185f67a291a0afe439ad14a1f54272b5487dc86
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-14 11:29:39 +0200

    feat(signup/page.tsx): implement a split-screen layout for the signup page to enhance user experience with visual elements and forms

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,3 +1,28 @@
+import PhotoBy from "@/app/components/auth/common/photoBy/photoBy";
+import Image from "next/image";
+import SplitScreen from "@/app/components/auth/common/splitScreen/splitScreen";
+import { SignUpForms } from "@/app/components/auth/signUpForms/signUpForms";
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <SplitScreen
+      left={
+        <div className="relative w-full h-full">
+          <Image
+            src="/images/auth/signup.jpg"
+            alt="Majorca, Spain"
+            fill
+            priority
+            sizes="(max-width: 768px) 0vw, 100vw"
+            className="object-cover"
+          />
+          <PhotoBy
+            location="Majorca, Spain"
+            photographer="Linus Nylund"
+            photoUrl="https://unsplash.com/@dreamsoftheoceans"
+          />
+        </div>
+      }
+      right={<SignUpForms />}
+    />
+  );
 }
