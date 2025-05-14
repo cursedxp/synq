8cf6f171f755c2d046c9f9ef4b3934b5232e423f
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-14 11:13:57 +0200

    refactor(signin): simplify SignIn component layout for better readability and maintainability fix(signInForm): update import paths for Button and Input components to reflect new directory structure

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -25,15 +25,7 @@ export default function SignIn() {
       }
       right={
         <div className="flex flex-1">
-          <div className="flex flex-col items-center justify-center w-full">
-            <div className="flex flex-col w-sm gap-2">
-              <h1 className="text-4xl font-bold text-left">Sign in</h1>
-              <p className="text-gray-500 mb-4">
-                Welcome back! Let&apos;s continue your journey
-              </p>
-              <SignInForm />
-            </div>
-          </div>
+          <SignInForm />
         </div>
       }
     />
--- app/components/auth/signInForm/signInForm.tsx
+++ app/components/auth/signInForm/signInForm.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Button from "../common/button/button";
-import Input from "../common/input/input";
+import Button from "@/app/components/auth/common/button/button";
+import Input from "@/app/components/auth/common/input/input";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signInSchema } from "@/app/schemas/signin/signin.schema";
@@ -33,41 +33,50 @@ export default function SignInForm() {
   };
 
   return (
-    <>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-4 w-full"
-      >
-        {/* Email */}
-        <Input
-          label="Email"
-          type="email"
-          placeholder="Email"
-          error={errors.email?.message}
-          {...register("email")}
-        />
-        {/* Password */}
-        <Input
-          label="Password"
-          type="password"
-          placeholder="Password"
-          error={errors.password?.message}
-          {...register("password")}
-        />
-        <Button label="Login" loading={loading} />
-      </form>
-      <div className="text-gray-500 text-sm flex justify-between">
-        <div>
-          Are you new here?{" "}
-          <Link href="/auth/signup" className="font-bold text-black underline">
-            Sign up
-          </Link>
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="flex flex-col w-sm gap-2">
+        <h1 className="text-4xl font-bold text-left">Sign in</h1>
+        <p className="text-gray-500 mb-4">
+          Welcome back! Let&apos;s continue your journey
+        </p>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4 w-full"
+        >
+          {/* Email */}
+          <Input
+            label="Email"
+            type="email"
+            placeholder="Email"
+            error={errors.email?.message}
+            {...register("email")}
+          />
+          {/* Password */}
+          <Input
+            label="Password"
+            type="password"
+            placeholder="Password"
+            error={errors.password?.message}
+            {...register("password")}
+          />
+          <Button label="Login" loading={loading} />
+        </form>
+        <div className="text-gray-500 text-sm flex justify-between">
+          <div>
+            Are you new here?{" "}
+            <Link
+              href="/auth/signup"
+              className="font-bold text-black underline"
+            >
+              Sign up
+            </Link>
+          </div>
+          <div className="flex text-sm text-black font-semibold underline">
+            <Link href="/auth/forgot-password">Forgot password?</Link>
+          </div>
         </div>
-        <div className="flex text-sm text-black font-semibold underline">
-          <Link href="/auth/forgot-password">Forgot password?</Link>
-        </div>
+        {error && <p className="text-red-500">{error}</p>}
       </div>
-      {error && <p className="text-red-500">{error}</p>}
-    </>
+    </div>
   );
 }
