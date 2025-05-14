deabe07f3a5d81690cb6782e9a3918f7c9a1e89b
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-14 14:50:03 +0200

    feat(signUpFormOne.tsx): add PasswordStrengthMeter component to enhance password security feedback for users during sign-up

--- app/components/auth/signUpForms/signUpFormOne.tsx
+++ app/components/auth/signUpForms/signUpFormOne.tsx
@@ -1,6 +1,7 @@
 import Input from "@/app/components/auth/common/input/input";
 import Button from "../common/button/button";
 import { Select } from "../common/select/select";
+import PasswordStrengthMeter from "../common/passwordStrengthMeter/PasswordStrengthMeter";
 export const SignUpFormOne = () => {
   return (
     <form className="flex flex-col gap-4 w-full">
@@ -11,6 +12,7 @@ export const SignUpFormOne = () => {
         placeholder="Password"
         name="password"
       />
+      <PasswordStrengthMeter />
       <Select label="Country of your business" />
       <Button label="Sign up" />
     </form>
