61611cb94c1bcddd5fcdea7cc3ca8854e44e9fbd
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-14 10:17:18 +0200

    feat(signin/page.tsx): implement SplitScreen component for improved layout and organization of sign-in page content

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import PhotoBy from "@/app/components/auth/common/photoBy/photoBy";
 import SignInForm from "@/app/components/auth/signInForm/signInForm";
+import SplitScreen from "@/app/components/auth/common/splitScreen/splitScreen";
+
 export default function SignIn() {
   return (
-    <div className="flex w-full h-screen">
-      <div className="hidden md:flex flex-1">
-        <div className="relative w-full h-full ">
+    <SplitScreen
+      left={
+        <div className="relative w-full h-full">
           <Image
             src="/images/auth/signin.jpg"
             alt="Yellow National Park"
             fill
-            className="object-cover"
+            priority
+            sizes="(max-width: 768px) 0vw, 100vw"
           />
           <PhotoBy
             location="Yellowstone National Park, United States"
@@ -18,18 +21,20 @@ export default function SignIn() {
             photoUrl="https://unsplash.com/@kimonmaritz"
           />
         </div>
-      </div>
-      <div className="flex flex-1">
-        <div className="flex flex-col items-center justify-center w-full ">
-          <div className="flex flex-col w-sm gap-2">
-            <h1 className="text-4xl font-bold text-left ">Sign in</h1>
-            <p className="text-gray-500 mb-4">
-              Welcome back! Let&apos;s continue your journey
-            </p>
-            <SignInForm />
+      }
+      right={
+        <div className="flex flex-1">
+          <div className="flex flex-col items-center justify-center w-full">
+            <div className="flex flex-col w-sm gap-2">
+              <h1 className="text-4xl font-bold text-left">Sign in</h1>
+              <p className="text-gray-500 mb-4">
+                Welcome back! Let&apos;s continue your journey
+              </p>
+              <SignInForm />
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      }
+    />
   );
 }
