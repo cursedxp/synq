9e53348bf3dfa13d7c7f83469b3fdd28d1ac5836
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-15 12:00:09 +0200

    feat(signUpFormOne.tsx): replace Select component with CountrySelect for better clarity and functionality feat(signUpFormOne.tsx): add sign-in link for better user navigation and experience style(signUpFormOne.tsx): update button label from "Sign up" to "Next" for improved user flow

--- app/components/auth/signUpForms/signUpFormOne.tsx
+++ app/components/auth/signUpForms/signUpFormOne.tsx
@@ -1,8 +1,8 @@
 import Input from "@/app/components/auth/common/input/input";
 import Button from "../common/button/button";
-import Select from "../common/select/select";
+import CountrySelect from "../common/select/countrySelect";
 import PasswordStrengthMeter from "../common/passwordStrengthMeter/passwordStrengthMeter";
-
+import Link from "next/link";
 const testOptions = [
   { label: "United States", value: "US" },
   { label: "Italy", value: "IT" },
@@ -12,17 +12,31 @@ const testOptions = [
 
 export const SignUpFormOne = () => {
   return (
-    <form className="flex flex-col gap-4 w-full">
-      <Input label="Email" type="email" placeholder="Email" name="email" />
-      <Input
-        label="Password"
-        type="password"
-        placeholder="Password"
-        name="password"
-      />
-      <PasswordStrengthMeter />
-      <Select label="Country of your business" options={testOptions} />
-      <Button label="Sign up" />
-    </form>
+    <>
+      <form className="flex flex-col gap-4 w-full">
+        <Input label="Email" type="email" placeholder="Email" name="email" />
+        <Input
+          label="Password"
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <PasswordStrengthMeter />
+        <CountrySelect
+          label="Country of your business"
+          options={testOptions}
+          className="pl-10"
+        />
+        <Button label="Next" />
+      </form>
+      <div className="text-gray-500 text-sm flex">
+        <div>
+          Already registered?{" "}
+          <Link href="/auth/signin" className="font-bold text-black underline">
+            Sign in
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
