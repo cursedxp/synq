939cbd651c9bed66d75fc6de1a018d98ce5c4e1c
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-15 11:19:17 +0200

    feat(signUpFormOne.tsx): add country selection options to the sign-up form to enhance user experience during registration

--- app/components/auth/signUpForms/signUpFormOne.tsx
+++ app/components/auth/signUpForms/signUpFormOne.tsx
@@ -1,7 +1,15 @@
 import Input from "@/app/components/auth/common/input/input";
 import Button from "../common/button/button";
-import { Select } from "../common/select/select";
+import Select from "../common/select/select";
 import PasswordStrengthMeter from "../common/passwordStrengthMeter/passwordStrengthMeter";
+
+const testOptions = [
+  { label: "United States", value: "US" },
+  { label: "Italy", value: "IT" },
+  { label: "Great Britain", value: "GB" },
+  { label: "Australia", value: "AU" },
+];
+
 export const SignUpFormOne = () => {
   return (
     <form className="flex flex-col gap-4 w-full">
@@ -13,7 +21,7 @@ export const SignUpFormOne = () => {
         name="password"
       />
       <PasswordStrengthMeter />
-      <Select label="Country of your business" />
+      <Select label="Country of your business" options={testOptions} />
       <Button label="Sign up" />
     </form>
   );
