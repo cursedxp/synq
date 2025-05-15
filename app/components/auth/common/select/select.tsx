0567ac57430a868e398904718edfd0a56c2a212e
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-15 11:19:08 +0200

    feat(select): implement a custom select component with country flags and dynamic options
    This change introduces a new Select component that allows users to select from a list of options, displaying corresponding country flags. It enhances the user experience by providing visual context for each option. The component also manages the selected state and updates accordingly when the user makes a selection.

--- app/components/auth/common/select/select.tsx
+++ app/components/auth/common/select/select.tsx
@@ -1,26 +1,58 @@
+"use client";
 import { FiChevronDown } from "react-icons/fi";
-
+import { useState, useRef } from "react";
+import Image from "next/image";
 interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   label: string;
   error?: string;
+  options?: { label: string; value: string }[];
 }
 
-export const Select = ({ label, ...props }: SelectProps) => {
+//TODO:Create json file with countries and their codes and flag file urls
+//TODO:Create a component that will fetch the data from the json file and display the data in the select
+
+export default function Select({ label, options, ...props }: SelectProps) {
+  const [currentOption, setCurrentOption] = useState(options?.[0]);
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedOption = options?.find(
+      (option) => option.value === e.target.value
+    );
+    setCurrentOption(selectedOption);
+  };
+
   return (
     <div>
       <label
         htmlFor="select"
-        className="text-sm text-black mb-1 cursor-pointer"
+        className="text-xs text-black mb-1 cursor-pointer"
       >
         {label}
       </label>
       <div className="relative">
+        <Image
+          src={`/flags/flag_${currentOption?.value}_16.svg`}
+          alt={currentOption?.label || ""}
+          width={16}
+          height={16}
+          style={{ width: "auto", height: "auto" }}
+          className="absolute left-3 top-3.5 flex items-center justify-center pointer-events-none"
+        />
         <select
           {...props}
           id={props?.name}
-          className="p-2 rounded-md border border-gray-300 w-full focus:outline-black appearance-none"
+          className="pr-2 pl-10 py-2 rounded-md border border-gray-300 w-full focus:outline-black appearance-none"
+          onChange={handleChange}
+          ref={selectRef}
         >
-          <option value="1">1</option>
+          {options?.map((option) => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            );
+          })}
         </select>
         <div className="absolute right-2 top-0 h-full flex items-center justify-center">
           <FiChevronDown />
@@ -28,4 +60,4 @@ export const Select = ({ label, ...props }: SelectProps) => {
       </div>
     </div>
   );
-};
+}
