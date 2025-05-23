8dc4b06210293411f5ac7dfd349eff20f3b28080
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-23 11:52:15 +0200

    feat(forgot-password): implement ForgotPasswordPage with SplitScreen layout and image integration for better user experience

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,3 +1,29 @@
-export default function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+import SplitScreen from "@/app/components/auth/common/splitScreen/splitScreen";
+import Image from "next/image";
+import PhotoBy from "@/app/components/auth/common/photoBy/photoBy";
+import ForgotPassword from "@/app/components/auth/forgotPassword/forgotPassword";
+
+export default function ForgotPasswordPage() {
+  return (
+    <SplitScreen
+      left={
+        <div className="relative w-full h-full">
+          <Image
+            src="/images/auth/forgotPassword.jpg"
+            alt="Don't forget to Live"
+            fill
+            priority
+            sizes="(max-width: 768px) 0vw, 100vw"
+            className="object-cover"
+          />
+          <PhotoBy
+            location="Lago Limides, BL, Italy"
+            photographer="Alessio Furlan"
+            photoUrl="https://unsplash.com/@alessiofurlan"
+          />
+        </div>
+      }
+      right={<ForgotPassword />}
+    ></SplitScreen>
+  );
 }
