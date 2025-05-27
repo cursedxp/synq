7cf41ffd0699ba50e12d925d6762dfb32a82b6f2
Anil Ozsoy <anilozsoy@gmail.com>
2025-05-27 11:52:41 +0200

    feat(verificationCard): add VerificationCard component for displaying verification status with visual feedback

---
+++ app/components/verification/verificationCard.tsx
@@ -0,0 +1,32 @@
+import { HiXMark, HiCheck } from "react-icons/hi2";
+
+type CardStatus = {
+  status: boolean | "error" | "success" | "loading";
+  message?: string;
+  title?: string;
+};
+
+export default function VerificationCard({ status, message }: CardStatus) {
+  return (
+    <>
+      <div className="flex justify-center mb-6">
+        <div
+          className={`w-32 h-32 rounded-full ${!status || status == "error" ? "bg-red-100" : "bg-green-100"} flex items-center justify-center`}
+        >
+          {!status || status == "error" ? (
+            <HiXMark className="text-red-500 text-6xl" />
+          ) : (
+            <HiCheck className="text-green-500 text-6xl" />
+          )}
+        </div>
+      </div>
+      <h2 className="text-2xl font-bold text-gray-900 mb-2">
+        {!status || status == "error"
+          ? "Verification Failed"
+          : "Verification Successful!"}
+      </h2>
+      <p className="text-gray-600 mb-4">{message}</p>
+      <p className="text-sm text-gray-500">Redirecting to sign up page...</p>
+    </>
+  );
+}
